c99071b2677361334759f531a0acf22fda906eee
Conor Grocock <grocock.conor@gmail.com>
2020-01-09 10:04:07 +0000

    Removed Docker

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,4 +1,12 @@
 import React, { Component } from 'react';
+import App from './screens/Home';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 class App extends Component {
     constructor(props) {
@@ -6,9 +14,25 @@ class App extends Component {
         this.state = {}
     }
     render() {
-        return (
-            <p>Test</p>
-        );
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     }
 }
 
---
+++ frontend/src/components/screens/Home.jsx
@@ -0,0 +1,13 @@
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <h1>Home</h1>
+        );
+    }
+}
+
+export default Home;
