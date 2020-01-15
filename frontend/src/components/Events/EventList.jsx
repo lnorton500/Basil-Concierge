7839fc5c2fe0a2fcd16dccec7b9c5e2ce1b82a91
Conor Grocock <grocock.conor@gmail.com>
2020-01-15 16:03:20 +0000

    Auto stash before rebase of "origin/master"

---
+++ frontend/src/components/Events/EventList.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { Waypoint } from 'react-waypoint';
+import axios from "axios";
+
+import Event from './Event';
+
+class EventList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            events: [],
+        }
+    }
+
+    componentDidMount() {
+        this.loadContent();
+    }
+
+    /**
+     * Load content using the Node Red api
+     */
+    loadContent() {
+        axios
+            .get("https://basil.eu-gb.mybluemix.net/api/events/", {
+                params: {
+                    skip: this.state.start,
+                    limit: this.state.limit
+                }
+            })
+            .then(res => {
+                this.setState({
+                    isLoaded: true,
+                    events: res.data
+                });
+            })
+            .catch(error => {
+                this.setState({
+                    error: error,
+                });
+            });
+    }
+
+    render() {
+        const { error, isLoaded, events } = this.state;
+        if (error)
+            return <div>Error: {error.message}</div>;
+        else if (!isLoaded)
+            return <div>Loading</div>;
+
+        return (
+            <div className="events">
+                {events.map((event, _i) => (
+                    <Event key={_i} event={event} onInterested={this.eventInterested} />
+                ))}
+                <Waypoint
+                    onEnter={this.loadContent}
+                    topOffset="50px" />
+            </div>
+        );
+    }
+}
+
+export default EventList;
