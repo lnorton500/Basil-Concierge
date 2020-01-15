e3bc021aec5ef0436a171213bf8eadc45cf50a93
Conor Grocock <grocock.conor@gmail.com>
2020-01-15 15:08:10 +0000

    Added selected results

--- frontend/src/components/Search/Search.jsx
+++ frontend/src/components/Search/Search.jsx
@@ -8,15 +8,35 @@ class Search extends Component {
         super(props);
         this.state = {
             show: true,
-            results: []
+            results: [],
+            selected: []
         }
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
         this.close = this.close.bind(this);
     }
 
+    handleClick(e) {
+        if (e === undefined) return
+        if (e in this.state.selected)
+            this.setState({
+                selected: this.state.selected.filter(function (selected) {
+                    return selected !== e
+                })
+            });
+        else {
+            this.setState({
+                selected: [...this.state.selected, e],
+                results: this.state.results.filter(function (result) {
+                    return result !== e
+                })
+            });
+        }
+    }
+
     handleChange(e) {
-        this.setState({ results: [...this.state.results, e.target.value] })
+        this.setState({ results: [e.target.value] })
     }
 
     close() {
@@ -34,8 +54,11 @@ class Search extends Component {
                     <input type="text" onChange={this.handleChange}></input>
                     <span className="exit" onClick={this.close}><i className="far fa-times-circle" style={{ color: "white" }}></i></span>
                     <div className="search-results">
+                        {this.state.selected.map((data, key) =>
+                            <SearchResult key={key} data={data} onClick={this.handleClick} selected />
+                        )}
                         {this.state.results.map((data, key) =>
-                            <SearchResult key={key} data={data} />
+                            <SearchResult key={key} data={data} onClick={this.handleClick} />
                         )}
                     </div>
                 </div>
