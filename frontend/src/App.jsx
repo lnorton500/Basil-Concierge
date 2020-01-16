ab0779e1d484ce8fb75ee41d090b078b206920ab
Conor Grocock <grocock.conor@gmail.com>
2020-01-16 00:22:57 +0000

    Added navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
+import { Route, NavLink, BrowserRouter as Router } from 'react-router-dom'
 
 import Home from './screens/Home';
-import UserCatagories from './components/UserStats/UserCatagories';
-import Search from './components/Search/Search';
+import Agenda from './screens/Agenda';
 
 import "./styles/css/app.css"
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+    }
 
     render() {
         return (
             <>
-                <div className="screen-content">
-                    <Home />
-                    <UserCatagories />
-                </div>
-                <Search />
+                <Router>
+                    <ul className="navigation">
+                        <li>
+                            <NavLink exact activeClassName="active" to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact activeClassName="active" to="/agenda">Agenda</NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact activeClassName="active" to="/books">Books</NavLink>
+                        </li>
+                    </ul>
+                    <div className="screen-content">
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/agenda" component={Agenda} />
+                    </div>
+                </Router>
             </>
         )
     }
